ecff1bae6417c97f1504d29328af1444ff954fbb
harcolemas@gmail.com <harcolemas@gmail.com>
2023-11-09 22:09:19 +0500

    Added NVIM for NVChad

---
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,51 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd" }
+
+for _, lsp in ipairs(servers) do
+    if lsp == "html" then
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  elseif lsp == "cssls" then
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  elseif lsp == "tsserver" then
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { "typescript-language-server", "--stdio" },
+    }
+  else
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+  }
+  end
+end
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+-- 
+-- lspconfig.pyright.setup { blabla}
---
+++ dot_config/nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,81 @@
+local M = {}
+
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+  autotag = {
+    enable = true,
+  }
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+    "codelldb",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+M.nvterm = {
+  cmd = "nvim",
+  dir = function ()
+    print("NIGGA")
+    return vim.fn.expand('%:p:h')
+  end,
+}
+
+function M.setup_autotag()
+  local status, autotag = pcall(require, "nvim-ts-autotag")
+  if not status then
+    return
+  end
+
+  autotag.setup({})
+end
+
+
+return M
